e29c1cd05066063cc7bb6dfa901540a7316f606b
obba achref <obbaachref178@gmail.com>
2025-11-16 15:26:37 +0100

    template integration and  some input control

---
+++ view/FrontOffice/js/signup_check.js
@@ -0,0 +1,48 @@
+submit=document.getElementById("submit").addEventListener('click',function verify(event){
+event.preventDefault();
+username=document.getElementById("name");
+username.placeholder="Enter your name";
+user_email=document.getElementById("email");
+user_email.placeholder="Enter your email";
+user_password=document.getElementById("password");
+user_password.placeholder="Enter your password";
+user_password_v=document.getElementById("verify_password");
+user_password_v.placeholder="Verify your password";
+user_Role=document.getElementById("role");
+user_Role.placeholder="Select your Role";
+submit=document.getElementById("submit");
+isValid=true;
+ function displayMessage(id, message, isError) {
+        var element = document.getElementById(id + "_error");
+        element.style.color = isError ? "red" : "green";
+        element.innerText = message;
+    }
+
+
+if(username.value.length===0 ){
+    displayMessage("username","U have to type a username",true);
+    isValid=false;
+
+}
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(user_email) || user_email.value.length==0) {
+    displayMessage("user_email","you have to type your email adress. please try again.",true);
+    isValid=false;
+  }
+if(user_password.value.length===0 ){
+    displayMessage("user_password","You have to chose a password",true);
+    isValid=false;
+
+}
+if( user_password.value !== user_password_v.value){
+     displayMessage("user_password_v","Wrong Password,Please try again",true);
+    isValid=false;
+
+}
+if(user_Role.value===" "){
+    displayMessage("user_Role","You have to chose a Role",true);
+    isValid=false;
+
+}
+});   
+
