4579a4adf800cc606e64f0b1b92ae96b1ba28e3b
Zinox55 <obbaachref178@gmail.com>
2025-11-17 23:29:42 +0100

    adding database file

---
+++ peaceconnect.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.11
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 17, 2025 at 11:28 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `peaceconnect`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cause`
+--
+
+CREATE TABLE `cause` (
+  `id_cause` int(20) NOT NULL,
+  `nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `image_url` varchar(155) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `don`
+--
+
+CREATE TABLE `don` (
+  `id_don` int(20) NOT NULL,
+  `montant` decimal(20,0) NOT NULL,
+  `devise` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `date_don` datetime(6) NOT NULL,
+  `donateur_nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `donateur_email` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `methode_paiments` int(20) NOT NULL,
+  `message` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sign_in`
+--
+
+CREATE TABLE `sign_in` (
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sign_up`
+--
+
+CREATE TABLE `sign_up` (
+  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `verify_password` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `sign_up`
+--
+
+INSERT INTO `sign_up` (`name`, `email`, `password`, `verify_password`) VALUES
+('', '', '', ''),
+('0', '0', '25', '25'),
+('achref obba', 'achrefka256@yahoo.fr', '256', '256');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cause`
+--
+ALTER TABLE `cause`
+  ADD PRIMARY KEY (`id_cause`);
+
+--
+-- Indexes for table `don`
+--
+ALTER TABLE `don`
+  ADD PRIMARY KEY (`id_don`);
+
+--
+-- Indexes for table `sign_up`
+--
+ALTER TABLE `sign_up`
+  ADD PRIMARY KEY (`name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
